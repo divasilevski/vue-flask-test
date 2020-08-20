7c8c27bcb228dc11c8d5d7982164ea4ca23c1ddf
Di <divasilevski@gmail.com>
2020-08-20 20:06:58 +0300

    дист

---
+++ dist/precache-manifest.78f7cfe176b72ed46787e82f16044fc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9367f60254adb769b73fdcb038e65bd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6f739fdb621e635c8c90",
+    "url": "/static/css/app.eca8e225.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/static/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "090cf37c80e8bec48971",
+    "url": "/static/js/about.b432873e.js"
+  },
+  {
+    "revision": "6f739fdb621e635c8c90",
+    "url": "/static/js/app.1f704a90.js"
+  },
+  {
+    "revision": "851f7408d13118b6017c",
+    "url": "/static/js/chunk-vendors.6020644f.js"
+  }
+]);
